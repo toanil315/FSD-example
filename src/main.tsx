da0d83c2b4360adeb04aa57d18d1e8b81b89b404
DANGCONGTOAN <dangcongtoan@DANGCONGTOANs-MacBook-Pro.local>
2024-06-10 10:32:33 +0700

    feat: integrate msw to mock APIs

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('@/app/configs/mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+});
